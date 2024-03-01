1f81b09a33e15b65c2f12be43ef40b4c68cff7e1
Anand3074 <subramanianand-cmpn@atharvacoe.ac.in>
2024-03-01 17:10:27 +0530

    app

--- MERN_BLOG/src/App.jsx
+++ MERN_BLOG/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function Home() {
-  return <h1>Home</h1>; // Define Home component
-}
+
 
 function App() {
   return (
-   <div>Hello World</div>
-  );
+          <>
+          <div>
+            HelloWorld
+          </div>
+          </>
+          
+    );
 }
 
 export default App;

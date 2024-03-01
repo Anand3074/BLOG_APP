5a0a606108749c773cb036e08d97d80d64775cac
Anand3074 <subramanianand-cmpn@atharvacoe.ac.in>
2024-03-01 15:09:54 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <div className="flex justify-center text-red-800 text-2xl font-bold h-screen bg-yellow-500 items-center">Hello World</div>
+  )
+}
+
+export default App

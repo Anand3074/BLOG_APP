971d73855c2befb1322463f0f66ada79b792d154
Anand3074 <subramanianand-cmpn@atharvacoe.ac.in>
2024-03-02 19:28:13 +0530

    created header page

--- App_Blog/src/App.jsx
+++ App_Blog/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
         <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/About' element={<About/>}/>
-        <Route path='/Dashboard' element={<Dashboard/>}/>
+        <Route path='/Projects' element={<Dashboard/>}/>
         </Routes>
    </BrowserRouter>
   )

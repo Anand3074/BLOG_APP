d74b3d0383cb7d33718f84343b8a35a71b1cb677
Anand3074 <subramanianand-cmpn@atharvacoe.ac.in>
2024-03-05 00:01:32 +0530

    create sign up page

--- App_Blog/src/App.jsx
+++ App_Blog/src/App.jsx
@@ -3,13 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Dashboard from './pages/Dash.jsx'
 import About from './pages/About.jsx'
+import SignUp from './pages/SignUp.jsx'
+import Header from './components/Header.jsx'
 const App = () => {
   return (
    <BrowserRouter>
+   <Header/>
         <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/About' element={<About/>}/>
         <Route path='/Projects' element={<Dashboard/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
         </Routes>
    </BrowserRouter>
   )

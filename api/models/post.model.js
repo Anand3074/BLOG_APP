cbc173b5bc26954dd762bcfd8750339aa5ee5e93
Anand3074 <subramanianand-cmpn@atharvacoe.ac.in>
2024-03-21 14:55:50 +0530

    created post create api route

---
+++ api/models/post.model.js
@@ -0,0 +1,77 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+    userId:{
+                type: String,
+                required:true,
+            },
+            content: {
+                type:String,
+                required:true,
+            },
+            title: {
+                type: String,
+                required:true,
+                unique:true,
+            },
+            image:{
+                type: String,
+                default: "https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post-768x337.webp",
+        
+            },
+            category:{
+                type:String,
+                default: 'uncategorised'
+            },
+            slug:{
+                type:String,
+                required:true,
+                unique:true,
+            }
+}
+,{
+    timestamps:true
+})
+
+const Post = mongoose.model("Post", postSchema)
+export default Post
+
+
+
+
+
+// const postSchema = (
+//     {
+//     userId:{
+//         type: String,
+//         required:true,
+//     },
+//     content: {
+//         type:String,
+//         required:true,
+//     },
+//     title: {
+//         type: String,
+//         required:true,
+//         unique:true,
+//     },
+//     image:{
+//         type: String,
+//         default: "https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post-768x337.webp",
+
+//     },
+//     category:{
+//         type:String,
+//         default: 'uncategorised'
+//     },
+//     slug:{
+//         type:String,
+//         required:true,
+//         unique:true,
+//     }
+// },
+// {
+//     timestamps : true
+// })
+// const Post = mangoose.model('Post', postSchema)
+// export default Post

b97b7bf2587b476e84abc90ff7d5eb8646b3bd7b
Anand3074 <subramanianand-cmpn@atharvacoe.ac.in>
2024-03-25 15:39:49 +0530

    added showmore functionality to the results of dashboard

--- App_Blog/src/components/DashPost.jsx
+++ App_Blog/src/components/DashPost.jsx
@@ -1,17 +1,22 @@
 import { useState, useEffect } from 'react';
 import {useSelector} from 'react-redux'
-import {Table} from 'flowbite-react'
+import {Button, Table} from 'flowbite-react'
 import {Link} from 'react-router-dom'
 const DashPost = () => {
   const {currentUser} = useSelector(state => state.user);
   const [userPosts, setUserPosts] = useState([])
+  const [showMore, setShowMore] = useState(true)
   useEffect(()=> {
     const fetchPosts = async () => {
       try {
         const res =await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        
         const data = await res.json();
         if(res.ok){
           setUserPosts(data.posts)
+          if(data.posts.length < 9){
+            setShowMore(false);
+          }
         }
       }   catch (error) {
         console.log(error.message)
@@ -21,7 +26,21 @@ const DashPost = () => {
       fetchPosts();
     }
   },[currentUser._id, currentUser.isAdmin])
-  console.log(userPosts)
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try { 
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      if(res.ok){
+        setUserPosts((prev) => [...prev, ...data.posts]);
+          if(data.posts.length < 9){
+            setShowMore(false)
+          }
+      }
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
 
     return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-400 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500  '>
@@ -70,6 +89,10 @@ const DashPost = () => {
           </Table.Body>
         ))}
       </Table>
+      {showMore ? (<button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+Show more
+      </button>) : <button className='w-full text-teal-500 self-center text-sm py-7'>
+        Show Less</button>}
       </>) :
     (<p>
       You have no posts</p>)} 

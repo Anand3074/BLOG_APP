d55dbd456e0d8c1006a087a383901b6766efc052
Anand3074 <subramanianand-cmpn@atharvacoe.ac.in>
2024-04-01 00:48:44 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth.route.js'
 import postRoutes from './routes/post.route.js'
 import commentRoutes from './routes/comment.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 mongoose
@@ -22,6 +23,8 @@ mongoose
     console.log(err);
 })
 
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -34,6 +37,13 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/App_Blog/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'App_Blog', 'dist', 'index.html'))
+});
+
 app.use((err,req, res, next) =>{
     const statusCode =err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
